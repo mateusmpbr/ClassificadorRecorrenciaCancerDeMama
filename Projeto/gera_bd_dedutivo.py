daca281cbca926516b9b2debed7ee55a402df728
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2019-11-13 14:16:11 -0300

    Up 'bd_dedutivo'

---
+++ Projeto/gera_bd_dedutivo.py
@@ -0,0 +1,16 @@
+with open('base_TP.txt','r') as f1:
+    with open('bd_dedutivo.txt','w+') as f2:
+        for line in f1:
+            
+            atributos = line.strip().split(',')
+            dados = ''
+            first = True
+
+            for atributo in atributos:
+                if first:
+                    dados+= f'\'{atributo}\''
+                    first = False
+                else:
+                    dados+= f',\'{atributo}\''
+
+            f2.write(f'individuo({dados})\n')
