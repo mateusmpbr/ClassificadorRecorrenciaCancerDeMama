763755fdf9035903748e101a5b7e663a726b3032
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2019-12-07 22:46:09 -0300

    Identifica a maioria de não-recorrências

--- Projeto/mapeia_atributos.py
+++ Projeto/mapeia_atributos.py
@@ -1,4 +1,4 @@
-with open('bd_dedutivo/padrao/bd_dedutivo_inteiro_python.txt','r') as f:
+with open('bd_dedutivo/nova_coluna/bd_dedutivo_inteiro_python_novaColuna.txt','r') as f:
     valor_total = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0],
@@ -23,7 +23,7 @@ with open('bd_dedutivo/padrao/bd_dedutivo_inteiro_python.txt','r') as f:
         [0, 0]
     ]
 
-    valor_nao_recorrencia_teste = [
+    valor_recorrencia = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -35,20 +35,36 @@ with open('bd_dedutivo/padrao/bd_dedutivo_inteiro_python.txt','r') as f:
         [0, 0]
     ]
 
+    teste_recorrencia_certo = 0
+    teste_recorrencia_errado = 0
+    teste_nao_recorrencia_certo = 0
+    teste_nao_recorrencia_errado = 0
+
     for index_line, line in enumerate(f):
         atributos = line.strip().split(',')
         for index_atributo, atributo in enumerate(atributos):
-            if (index_atributo == len(atributos)-1):
+            if (index_atributo == len(atributos) - 1):
                 break
+
             atributo_aux = int(atributo) - 1
 
             valor_total[index_atributo][atributo_aux] += 1
-            # print("recorrente?",atributos[len(atributos)-1])
-            if(atributos[len(atributos)-1] == '0'):
+            # print("recorrente?",atributos[-1])
+            if(atributos[-1] == '0'):
                 valor_nao_recorrencia[index_atributo][atributo_aux] += 1
+            else:
+                valor_recorrencia[index_atributo][atributo_aux] += 1
 
-            if(int(atributos[0]) > 5 or int(atributos[2]) < 5):
-                valor_nao_recorrencia_teste[index_atributo][atributo_aux] += 1
+        if(atributos[-2] == '2' or atributos[-5] == '1'):
+            if(atributos[-1] == '0'):
+                teste_nao_recorrencia_certo+=1
+            else:
+                teste_recorrencia_errado+=1
+        else:
+            if(atributos[-1] == '1'):
+                teste_recorrencia_certo+=1
+            else:
+                teste_nao_recorrencia_errado+=1
 
     nome_atributos = ['idade', 'menopausa', 'tamanho do tumor', 'inv-nodes',
                       'node-caps', 'deg-malig', 'mama', 'breast-quad', 'irradiada']
@@ -56,5 +72,11 @@ with open('bd_dedutivo/padrao/bd_dedutivo_inteiro_python.txt','r') as f:
     for i in range(9):
         print('-----', nome_atributos[i], '-----')
         print("VALOR TOTAL:", valor_total[i])
+        print("VALOR RECORRENCIA", valor_recorrencia[i])
         print("VALOR NAO RECORRENCIA", valor_nao_recorrencia[i])
-        print("VALOR NAO RECORRENCIA TESTE", valor_nao_recorrencia_teste[i])
+    
+    print('------------------------------')
+    print("TESTE RECORRENCIA CERTO", teste_recorrencia_certo)
+    print("TESTE RECORRENCIA ERRADO", teste_recorrencia_errado)
+    print("TESTE NAO RECORRENCIA CERTO", teste_nao_recorrencia_certo)
+    print("TESTE NAO RECORRENCIA ERRADO", teste_nao_recorrencia_errado)
